e45c36a4ed94eb04745bc063a14e458347fd80ba
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-01-12 20:22:54 +0530

    Today 12/01/2024 Leetcode POD

---
+++ Leetcode_Jan2024/StringAlike.cpp
@@ -0,0 +1,84 @@
+/*
+1704. Determine if String Halves Are Alike
+
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool halvesAreAlike(string s)
+    {
+        int n = s.size();
+        int p = n / 2;
+        int m = 0, k = 0;
+        for (int i = 0; i < p; i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            {
+                m++;
+            }
+        }
+        for (int i = p; i < n; i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            {
+                k++;
+            }
+        }
+        return m == k ? true : false;
+    }
+};
+
+/*
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        auto countVowels = [](const string& str) {
+            unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+            int count = 0;
+            for (char c : str) {
+                if (vowels.count(c) > 0) {
+                    count++;
+                }
+            }
+            return count;
+        };
+
+        int length = s.length();
+        int midPoint = length / 2;
+
+        string firstHalf = s.substr(0, midPoint);
+        string secondHalf = s.substr(midPoint);
+
+        return countVowels(firstHalf) == countVowels(secondHalf);
+    }
+};
+*/
